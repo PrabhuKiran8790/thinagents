f39ecac13498e0bfc6e46e9599a18ec4200890c8
Prabhu Kiran Konda <prabhukiran8790@pm.me>
2025-05-26 18:18:54 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="thinagents",
-    version="0.0.1",
+    version="0.0.2",
     author="Prabhu Kiran Konda",
     description="A lightweight AI Agent framework",
     packages=find_packages(exclude=["tests", "tests.*"]),
     python_requires=">=3.11",
     install_requires=[
-        "litellm",
-        "graphviz"
+        "litellm>=1.70.0",
+        "graphviz>=0.20.0"
+    ],
+    setup_requires=[
+        "litellm>=1.70.0",
+        "graphviz>=0.20.0"
     ],
 )
